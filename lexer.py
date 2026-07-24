3ee0b99a6cd3169458d2fa4702176eaade077e9d
Steve Kaser <steve.kaser@nkparts.com>
2026-07-23 22:11:18 -0400

    rework

--- lexer.py
+++ lexer.py
@@ -1,21 +1,18 @@
 from utils import *
 
-if __name__ == "__main__":
-    fp = Path("./test.zplp")
+def lex_file(fp: Path) -> Lexer:
+    lex = Lexer(fp)
     lines = get_file_data(fp)
-    lex: list[Line] = []
     for i, line in enumerate(lines):
         line = line.strip()
         if len(line) == 0:
             continue
         if line.startswith("DEF"):
             _, var, value = line.split(" ")
-            lex.append(Def_Line(Line_Type.DEF, line, i+1, var, value))
+            lex.lines.append(Def_Line(Line_Type.DEF, line, i+1, var, value))
         elif line.startswith("BEGIN_"):
             if line.startswith("BEGIN_DOCUMENT"):
                 block_type = Block_Type.DOCUMENT
-            elif line.startswith("BEGIN_SETUP"):
-                block_type = Block_Type.SETUP
             elif line.startswith("BEGIN_SECTION"):
                 block_type = Block_Type.SECTION
             elif line.startswith("BEGIN_FIELD"):
@@ -25,12 +22,10 @@ if __name__ == "__main__":
             label = ""
             if ":" in line:
                 label = ''.join(line.split(":")[1:])
-            lex.append(Block_Begin_Line(Line_Type.BLOCK_BEGIN, line, i+1, block_type, label))
+            lex.lines.append(Block_Begin_Line(Line_Type.BLOCK_BEGIN, line, i+1, block_type, label))
         elif line.startswith("END_"):
             if line.startswith("END_DOCUMENT"):
                 block_type = Block_Type.DOCUMENT
-            elif line.startswith("END_SETUP"):
-                block_type = Block_Type.SETUP
             elif line.startswith("END_SECTION"):
                 block_type = Block_Type.SECTION
             elif line.startswith("END_FIELD"):
@@ -40,14 +35,13 @@ if __name__ == "__main__":
             label = ""
             if ":" in line:
                 label = ''.join(line.split(":")[1:])
-            lex.append(Block_End_Line(Line_Type.BLOCK_END, line, i+1, block_type, label))
+            lex.lines.append(Block_End_Line(Line_Type.BLOCK_END, line, i+1, block_type, label))
         elif line.startswith("//"):
-            lex.append(Line(Line_Type.COMMENT, line, i+1))
+            lex.lines.append(Line(Line_Type.COMMENT, line, i+1))
         else:
             first_space = line.index(" ")
             param = line[0:first_space]
             value = line[first_space+1:]
-            lex.append(Parameter_Line(Line_Type.PARAMETER, line, i+1, param, value))
+            lex.lines.append(Parameter_Line(Line_Type.PARAMETER, line, i+1, param, value))
 
-    for l in lex:
-        print(l)
+    return lex
--- utils.py
+++ utils.py
@@ -2,7 +2,57 @@ from enum import Enum
 from pathlib import Path
 from dataclasses import dataclass, field
 
-DEFINES = {}
+def get_file_data(fp: Path) -> list[str]:
+    lines = []
+    with open(str(fp), "r") as f:
+        lines = f.readlines()
+    return lines
+
+def is_numeric(value: str) -> bool:
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+def validate_min_max_int(val: int, min_val: int, max_val: int, default_val: int) -> tuple[bool, str, int]:
+    if val < min_val:
+        return (False, f"Value {val} is less than minimum value ({min_val})!", default_val)
+    if val > max_val:
+        return (False, f"Value {val} is greater than minimum value ({min_val})!", default_val)
+    return (True, "", val)
+
+def validate_min_max_float(val: float, min_val: float, max_val: float, default_val: float) -> tuple[bool, str, float]:
+    if val < min_val:
+        return (False, f"Value {val} is less than minimum value ({min_val})!", default_val)
+    if val > max_val:
+        return (False, f"Value {val} is greater than minimum value ({min_val})!", default_val)
+    return (True, "", val)
+
+def validate_min_max_step_int(val: int, min_val: int, max_val: int, step_val: int, default_val: int) -> tuple[bool, str, int]:
+    if val < min_val:
+        return (False, f"Value {val} is less than minimum value ({min_val})!", default_val)
+    if val > max_val:
+        return (False, f"Value {val} is greater than minimum value ({min_val})!", default_val)
+    # TODO: figure out how to determine if the step is being respected
+    return (True, "", val)
+
+def validate_min_max_step_float(val: float, min_val: float, max_val: float, step_val: float, default_val: float) -> tuple[bool, str, float]:
+    if val < min_val:
+        return (False, f"Value {val} is less than minimum value ({min_val})!", default_val)
+    if val > max_val:
+        return (False, f"Value {val} is greater than minimum value ({min_val})!", default_val)
+    # TODO: figure out how to determine if the step is being respected
+    return (True, "", val)
+
+def validate_list(val: str, options: list[str], default_val: str) -> tuple[bool, str, str]:
+    if val not in options:
+        return (False, f"Value {val} not in the provided options: {''.join(options)}", default_val)
+    return (True, "", val)
+
+class ZPL_Operator(Enum):
+    CARET = 0
+    TILDE = 1
 
 class ZPL_Field_Type(Enum):
     TEXT = 0
@@ -41,54 +91,65 @@ class ZPL_Barcode_Type(Enum):
 class ZPL_Linear_Barcode_Type(Enum):
     CODE39 = 0
 
-@dataclass
-class ZPL_Setup_Command:
-    ...
+class Line_Type(Enum):
+    DEF = 0
+    BLOCK_BEGIN = 1
+    BLOCK_END = 2
+    COMMENT = 3
+    PARAMETER = 4
+
+class Block_Type(Enum):
+    DOCUMENT = 0
+    SECTION = 1
+    FIELD = 2
+
+class ZPL_Parameter_Type(Enum):
+    STRING = 0
+    INTEGER = 1
+    FLOAT = 2
+
+class ZPL_Parameter_Validation_Type(Enum):
+    NONE = 0
+    MIN_MAX = 1
+    MIN_MAX_STEP = 2
+    LIST = 3
+    COMMAND = 4
+
+@dataclass(kw_only=True)
+class ZPL_Parameter:
+    name: str
+    type: ZPL_Parameter_Type
+    validation: ZPL_Parameter_Validation_Type
 
 @dataclass
-class ZPL_Print_Width(ZPL_Setup_Command):
-    width: int = 2
-    operator: str = "^PW"
+class ZPL_String_Parameter(ZPL_Parameter):
+    default_value: str
+    type: ZPL_Parameter_Type = ZPL_Parameter_Type.STRING
+    validation: ZPL_Parameter_Validation_Type = ZPL_Parameter_Validation_Type.LIST
+    valid_options: list[str] = field(default_factory=list)
 
 @dataclass
-class ZPL_ZPL_Mode(ZPL_Setup_Command):
-    mode: int = 2
-    operator: str = "^SZ"
+class ZPL_Integer_Parameter(ZPL_Parameter):
+    default_value: int
+    type: ZPL_Parameter_Type = ZPL_Parameter_Type.INTEGER
+    min: int = 0
+    max: int = 0
+    valid_options: list[int] = field(default_factory=list)
 
 @dataclass
-class ZPL_Dots_Per_MM(ZPL_Setup_Command):
-    dots: str = "A"
-    operator: str = "^JM"
+class ZPL_Float_Parameter(ZPL_Parameter):
+    default_value: float
+    type: ZPL_Parameter_Type = ZPL_Parameter_Type.FLOAT
+    min: int = 0
+    max: int = 0
+    valid_options: list[float] = field(default_factory=list)
 
-@dataclass
-class ZPL_Bitmap_Clear(ZPL_Setup_Command):
-    should_clear: str = "Y"
-    operator: str = "^MC"
-
-@dataclass
-class ZPL_Mirror_Image(ZPL_Setup_Command):
-    should_mirror: str = "N"
-    operator: str = "^PM"
-
-@dataclass
-class ZPL_Backfeed_Seq(ZPL_Setup_Command):
-    backfeed_amount: ZPL_Backfeed_Amount
-    operator: str = "~JS"
-
-@dataclass
-class ZPL_Reprint_After_Error(ZPL_Setup_Command):
-    should_reprint_after_error: str = "Y"
-    operator: str = "^JZ"
-
-@dataclass
-class ZPL_Label_Home(ZPL_Setup_Command):
-    home: tuple[int, int] = (0, 0)
-    operator: str = "^LH"
-
-@dataclass
-class ZPL_Reverse_Print(ZPL_Setup_Command):
-    should_reverse_print: str = "N"
-    operator: str = "^LR"
+@dataclass(kw_only=True)
+class ZPL_Command:
+    operator: ZPL_Operator
+    mnemonic: str
+    keyword: str
+    parameters: list[ZPL_Parameter] = field(default_factory=list)
 
 @dataclass
 class ZPL_Font:
@@ -108,9 +169,9 @@ class ZPL_Field_Position:
     y: int = 0
     z: int = 0
 
-@dataclass
-class ZPL_Field_Origin(ZPL_Field_Position):
-    operator: str = "^FO"
+# @dataclass
+# class ZPL_Field_Origin(ZPL_Field_Position):
+#     operator: str = "^FO"
 
 @dataclass
 class ZPL_Text_Field_Origin(ZPL_Field_Position):
@@ -127,59 +188,41 @@ class ZPL_Text_Field(ZPL_Field):
     text: ZPL_Text
     type: ZPL_Field_Type = ZPL_Field_Type.TEXT
 
-@dataclass
-class ZPL_Linear_Barcode_Config():
-    module_width: int = 2
-    bar_width_ratio: float = 3.0
-    module_height: int = 10
-    type: ZPL_Field_Type = ZPL_Field_Type.BARCODE
-    barcode_type: ZPL_Barcode_Type = ZPL_Barcode_Type.LINEAR
+# @dataclass
+# class ZPL_Linear_Barcode_Config():
+#     module_width: int = 2
+#     bar_width_ratio: float = 3.0
+#     module_height: int = 10
+#     type: ZPL_Field_Type = ZPL_Field_Type.BARCODE
+#     barcode_type: ZPL_Barcode_Type = ZPL_Barcode_Type.LINEAR
 
-class ZPL_Linear_Barcode_Code39(ZPL_Field):
-    config: ZPL_Linear_Barcode_Config = ZPL_Linear_Barcode_Config()
-    linear_barcode_type: ZPL_Linear_Barcode_Type = ZPL_Linear_Barcode_Type.CODE39
-    orientation: ZPL_Field_Orientation = ZPL_Field_Orientation.NORMAL
-    use_mod43_check_digit: str = "N"
-    height: int = 1
-    print_value_below_barcode: str = "Y"
-    print_value_above_barcode: str = "N"
-    operator: str = "^B3"
+# class ZPL_Linear_Barcode_Code39(ZPL_Field):
+#     config: ZPL_Linear_Barcode_Config = ZPL_Linear_Barcode_Config()
+#     linear_barcode_type: ZPL_Linear_Barcode_Type = ZPL_Linear_Barcode_Type.CODE39
+#     orientation: ZPL_Field_Orientation = ZPL_Field_Orientation.NORMAL
+#     use_mod43_check_digit: str = "N"
+#     height: int = 1
+#     print_value_below_barcode: str = "Y"
+#     print_value_above_barcode: str = "N"
+#     operator: str = "^B3"
 
-@dataclass
-class ZPL_Graphic_Field(ZPL_Field):
-    type: ZPL_Field_Type = ZPL_Field_Type.GRAPHIC
+# @dataclass
+# class ZPL_Graphic_Field(ZPL_Field):
+#     type: ZPL_Field_Type = ZPL_Field_Type.GRAPHIC
 
-@dataclass
-class ZPL_Graphic_Box_Field(ZPL_Graphic_Field):
-    width: int = 1
-    height: int = 1
-    thickness: int = 1
-    line_color: str = "B"
-    deg_of_corner_rounding: int = 0
-    graphic_type: ZPL_Graphic_Type = ZPL_Graphic_Type.BOX
+# @dataclass
+# class ZPL_Graphic_Box_Field(ZPL_Graphic_Field):
+#     width: int = 1
+#     height: int = 1
+#     thickness: int = 1
+#     line_color: str = "B"
+#     deg_of_corner_rounding: int = 0
+#     graphic_type: ZPL_Graphic_Type = ZPL_Graphic_Type.BOX
 
 @dataclass
 class ZPL_Section:
     label: str
     fields: list[ZPL_Field] = field(default_factory=list)
-
-@dataclass
-class ZPL_Document:
-    setup: list[ZPL_Setup_Command] = field(default_factory=list)
-    sections: list[ZPL_Section] = field(default_factory=list)
-
-class Line_Type(Enum):
-    DEF = 0
-    BLOCK_BEGIN = 1
-    BLOCK_END = 2
-    COMMENT = 3
-    PARAMETER = 4
-
-class Block_Type(Enum):
-    DOCUMENT = 0
-    SETUP = 1
-    SECTION = 2
-    FIELD = 3
 
 @dataclass
 class Line:
@@ -206,25 +249,22 @@ class Block_End_Line(Line):
 class Parameter_Line(Line):
     param_name: str
     value: str
-    
-def get_file_data(fp: Path) -> list[str]:
-    lines = []
-    with open(str(fp), "r") as f:
-        lines = f.readlines()
-    return lines
 
-def is_numeric(value: str) -> bool:
-    try:
-        float(value)
-        return True
-    except ValueError:
-        return False
-    
-def handle_def(line: str) -> None:
-    line = line[4:]
-    chunks = line.split(" ")
-    var = chunks.pop(0)
-    value = ''.join(chunks)
-    if var in DEFINES:
-        raise KeyError(f"Key with name {var} already exists!")
-    DEFINES[var] = value
+@dataclass
+class Lexer:
+    file_path: Path
+    lines: list[Line] = field(default_factory=list)
+
+@dataclass
+class ZPL_Document:
+    sections: list[ZPL_Section] = field(default_factory=list)
+    defines: dict[str, str] = field(default_factory=dict)
+
+    def parse_def_line(self, line: Def_Line) -> None:
+        line.__class__ = Def_Line
+        if line.var_name in self.defines:
+            raise KeyError(f"Line {line.number}: {line.var_name} is already defined!")
+        self.defines[line.var_name] = line.value
+
+    def parse_begin_document_line(self, line: Block_Begin_Line) -> None:
+        ...
